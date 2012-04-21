5bc04db89c8e38de7849fabb4778fbe84c07746b
Philipp Küng <i@philippkueng.ch>
2012-04-21 14:21:06 +0200

    bulk insert of files from shakty/nodeGame, will be needed for running tests and distributing a set of standard games

---
+++ games/ultimatum/logic.js
@@ -0,0 +1,13 @@
+var nodegame = require('../node_modules/nodegame-server/node_modules/nodegame-client');
+module.exports.nodegame = nodegame;
+
+var Ultimatum = require('ultimatum');
+
+var conf = {
+  	name: "Ultimatum_Logic",
+	url: "http://localhost:8080/ultimatum/admin"
+    // verbosity: 0
+    // verbosity: 10
+};
+
+nodegame.play(conf, new Ultimatum());
