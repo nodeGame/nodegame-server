c482fabb3da8c720e627ed3e7eec9a7029fb2d7b
Stefano Balietti <futur.dorko@gmail.com>
2012-10-01 23:20:03 +0200

    configuration for nodegame-server, express and socket.io is read from file

---
+++ conf/servernode.js
@@ -0,0 +1,7 @@
+module.exports = configure;
+
+
+function configure (servernode) {
+		
+	return true;
+}
---
+++ conf/sio.js
@@ -0,0 +1,6 @@
+module.exports = configure;
+
+function configure (sio, servernode) {
+    sio.enable('browser client etag');
+    sio.set('log level', -1);
+}
