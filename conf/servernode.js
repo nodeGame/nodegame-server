9a1dcf27b37eebbef1000783054a6250a589fe38
Stefano Balietti <futur.dorko@gmail.com>
2012-10-13 14:39:29 +0200

    changed how options are given in input

--- conf/servernode.js
+++ conf/servernode.js
@@ -2,6 +2,39 @@ module.exports = configure;
 
 
 function configure (servernode) {
-		
+	
+	var rootDir = path.resolve(__dirname, '..');
+    
+	servernode.name = "nodeGame server";
+	servernode.verbosity = 0;
+	
+	servernode.log = {};
+	servernode.log.msg = false;
+	servernode.log.sys = false;
+	
+	
+	
+    
+	servernode.mail = false;
+	
+
+	servernode.defaultGamesDir = rootDir + '/games/';
+	servernode.gamesDirs = [this.defaultGamesDir];
+    
+	if (process && process.env.PORT){
+		servernode.port = process.env.PORT; // if app is running on heroku then the assigned port has to be used.
+	} else {
+		servernode.port = '80'; // port of the express server and sio
+	}
+	 
+	
+	servernode.maxChannels = 0; // unlimited
+	
+	servernode.maxListeners = 0; // unlimited
+	
+	servernode.defaults.channel = {};
+	
+	
+	
 	return true;
 }
