2ca090acec767ed83234a6a489aee8d5f74fc3bf
Stefano Balietti <futur.dorko@gmail.com>
2012-12-01 18:25:11 +0100

    minor

--- public/javascripts/nodegame.js
+++ public/javascripts/nodegame.js
@@ -152,6 +152,70 @@ if (JSUS.isNodeJS()) {
 
 
 /**
+ * # SUPPORT
+ *  
+ * Copyright(c) 2012 Stefano Balietti
+ * MIT Licensed
+ * 
+ * Tests browsers ECMAScript 5 compatibility
+ * 
+ * For more information see http://kangax.github.com/es5-compat-table/
+ * 
+ */
+
+(function (JSUS) {
+    
+function COMPATIBILITY() {};
+
+/**
+ * ## COMPATIBILITY.compatibility
+ * 
+ * Returns a report of the ECS5 features available
+ * 
+ * Useful when an application routinely performs an operation 
+ * depending on a potentially unsupported ECS5 feature. 
+ * 
+ * Transforms multiple try-catch statements in a if-else
+ * 
+ * @return {object} support The compatibility object
+ */
+COMPATIBILITY.compatibility = function() {
+
+	var support = {};
+	
+	try {
+		Object.defineProperty({}, "a", {enumerable: false, value: 1})
+		support.defineProperty = true;
+	}
+	catch(e) {
+		support.defineProperty = false;	
+	}
+	
+	try {
+		eval('({ get x(){ return 1 } }).x === 1')
+		support.setter = true;
+	}
+	catch(err) {
+		support.setter = false;
+	}
+	  
+	try {
+		var value;
+		eval('({ set x(v){ value = v; } }).x = 1');
+		support.getter = true;
+	}
+	catch(err) {
+		support.getter = false;
+	}	  
+
+	return support;
+};
+
+
+JSUS.extend(COMPATIBILITY);
+    
+})('undefined' !== typeof JSUS ? JSUS : module.parent.exports.JSUS);
+/**
  * # ARRAY
  *  
  * Copyright(c) 2012 Stefano Balietti
