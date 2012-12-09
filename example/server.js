6f283ba8ca6cf032f3c3b7cef9788a717c377e6a
Stefano Balietti <futur.dorko@gmail.com>
2012-12-09 22:37:02 +0100

    modified

--- example/server.js
+++ example/server.js
@@ -2,36 +2,17 @@
  * Start File for nodeGame server with conf.
  */
 
-var ServerNode = require('../ServerNode');
+var ServerNode = require('../index').ServerNode;
 
-var options = {
-    name: "nodeGame Server",
-    port: 8080,
-    verbosity: 10,
-    dumpsys: false,
-    dumpmsg: false,
-    mail: false,
-    io: { 
-        set: {
-            'transports': ['xhr-polling'],
-            'polling duration': 10,
-            // transports: ['websocket'],
-            'log level': -1
-        }
-    },
-    http: {}
-};
-
-
-
-// ServerNode accepts two additional parameters:
-// - an instance of an http Express server
-// - an instance of socket.io
-// If not passed, they will be created with default settings
-var sn = new ServerNode(options);
+var sn = new ServerNode();
 
 sn.addChannel({
     name: 'Ultimatum',
     admin: 'ultimatum/admin',
     player: 'ultimatum'
 });
+
+
+var ultimatum = require('./games/ultimatum/server/logic.js');
+sn.startGame('ultimatum', ultimatum);
+
