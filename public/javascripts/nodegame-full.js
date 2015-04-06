b2e6982ce3c5c76ae7af0bcbe6c3fa4bb69d90e3
Stefano Balietti <futur.dorko@gmail.com>
2015-04-06 15:47:33 +0200

    rebuilt

--- public/javascripts/nodegame-full.js
+++ public/javascripts/nodegame-full.js
@@ -24269,9 +24269,9 @@ if (!Array.prototype.indexOf) {
         if (func) func.call(node.game);
 
         // ng event emitter is not used.
-        node.events.game.emit('FRAME_LOADED');
-        node.events.stage.emit('FRAME_LOADED');
-        node.events.step.emit('FRAME_LOADED');
+        node.events.ee.game.emit('FRAME_LOADED');
+        node.events.ee.stage.emit('FRAME_LOADED');
+        node.events.ee.step.emit('FRAME_LOADED');
 
         if (loaded) {
             stageLevel = node.game.getStageLevel();
