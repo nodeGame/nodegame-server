5fa7a57650d685d68ecd1e6d6f0a3a9d67d87e06
Stefano Balietti <futur.dorko@gmail.com>
2015-07-16 17:12:12 +0200

    minor

--- phantomjs/openurl.js
+++ phantomjs/openurl.js
@@ -1,47 +1,38 @@
-console.log('0');
+/**
+ * # openurl
+ * Copyright(c) 2015 Stefano Balietti
+ * MIT Licensed
+ *
+ * Open a phantomjs instance to the specified url
+ *
+ * This file is executed via `#ServerChannel.connectPhantom()`.
+ *
+ * http://www.nodegame.org
+ */
+
+"use strict";
 
 var webpage = require('webpage');
 var system = require('system');
 
-console.log('1');
-
-var url;
+var url, page;
 
 if (system.args.length < 2) {
     console.log('Need URL!');
     phantom.exit();
 }
-
 url = system.args[1];
 
-console.log('2');
-
-var page = webpage.create();
-
-console.log('3');
+page = webpage.create();
 
 page.onConsoleMessage = function(msg) {
-
-    console.log(msg);
-
     if (msg === 'PHANTOMJS EXITING') phantom.exit();
     console.log(msg);
 };
 
-console.log('4');
-
 page.open(url, function() {
-
-console.log('5');
-
     setTimeout(function() {
-
-        console.log('6');
-
         page.evaluate(function() {
-
-            console.log('7');
-
             node.events.ee.ng.on(
                 'GAME_OVER', function() { console.log('PHANTOMJS EXITING'); });
         });
