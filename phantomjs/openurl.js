b3fea91880ed98f172098b212b581d6ac6afa181
Stefano Balietti <futur.dorko@gmail.com>
2015-07-16 15:33:46 +0200

    debug for travis

--- phantomjs/openurl.js
+++ phantomjs/openurl.js
@@ -1,6 +1,8 @@
 var webpage = require('webpage');
 var system = require('system');
 
+console.log('1');
+
 var url;
 
 if (system.args.length < 2) {
@@ -10,14 +12,34 @@ if (system.args.length < 2) {
 
 url = system.args[1];
 
+console.log('2');
+
 var page = webpage.create();
+
+console.log('3');
+
 page.onConsoleMessage = function(msg) {
+
+    console.log(msg);
+
     if (msg === 'PHANTOMJS EXITING') phantom.exit();
     console.log(msg);
 };
+
+console.log('4');
+
 page.open(url, function() {
+
+console.log('5');
+
     setTimeout(function() {
+
+        console.log('6');
+
         page.evaluate(function() {
+
+            console.log('7');
+
             node.events.ee.ng.on(
                 'GAME_OVER', function() { console.log('PHANTOMJS EXITING'); });
         });
