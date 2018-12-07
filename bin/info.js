970f86ad4691c81df103e5b555a358066785a032
Stefano Balietti <stbaliet@microsoft.com>
2018-12-07 10:39:39 -0500

    returning versions to monitor

--- bin/info.js
+++ bin/info.js
@@ -3,12 +3,23 @@
 const J = require('JSUS').JSUS;
 const path = require('path');
 
+// Directories.
+
 let ngcDir = J.resolveModuleDir('nodegame-client', __dirname);
 let JSUSDir = J.resolveModuleDir('JSUS', __dirname);
 let NDDBDir = J.resolveModuleDir('NDDB', __dirname);
 let shelfDir = J.resolveModuleDir('shelf.js', __dirname);
 let ngwindowDir = J.resolveModuleDir('nodegame-window', __dirname);
 let ngwidgetsDir = J.resolveModuleDir('nodegame-widgets', __dirname);
+let monitorDir = J.resolveModuleDir('nodegame-monitor', __dirname);
+let requirementsDir = J.resolveModuleDir('nodegame-requirements', __dirname);
+let mturkDir = J.resolveModuleDir('nodegame-mturk', __dirname);
+let gametemplateDir = J.resolveModuleDir('nodegame-game-template', __dirname);
+let generatorDir = J.resolveModuleDir('nodegame-generator', __dirname);
+let expressDir = J.resolveModuleDir('express', __dirname);
+let socketioDir = J.resolveModuleDir('socket.io', __dirname);
+
+// Build Scripts.
 
 let buildClient = require(path.resolve(ngcDir, 'bin', 'build.js')).build;
 let buildClientSup =
@@ -20,6 +31,28 @@ let buildNgWindow = require(path.resolve(ngwindowDir, 'bin', 'build.js')).build;
 let buildNgWidgets =
     require(path.resolve(ngwidgetsDir, 'bin', 'build.js')).build;
 let buildCSS = require('./buildCSS');
+
+// Express and Socket.io
+
+let pkgExpress = require(path.resolve(expressDir, 'package.json'));
+let pkgSocketio = require(path.resolve(socketioDir, 'package.json'));
+
+// Packages.
+
+let pkgClient = require(path.resolve(ngcDir, 'package.json'));
+let pkgJSUS = require(path.resolve(JSUSDir, 'package.json'));
+let pkgNDDB = require(path.resolve(NDDBDir, 'package.json'));
+// let pkgShelf = require(path.resolve(shelfDir, 'package.json'));
+let pkgNgWindow = require(path.resolve(ngwindowDir, 'package.json'));
+let pkgNgWidgets = require(path.resolve(ngwidgetsDir, 'package.json'));
+let pkgMonitor = require(path.resolve(monitorDir, 'package.json'));
+let pkgRequirements = require(path.resolve(requirementsDir, 'package.json'));
+let pkgMturk = require(path.resolve(mturkDir, 'package.json'));
+let pkgGameTemplate = require(path.resolve(gametemplateDir, 'package.json'));
+let pkgGenerator = require(path.resolve(generatorDir, 'package.json'));
+
+
+// Server Folders.
 
 let rootDir = path.resolve(__dirname, '..');
 let buildDir = path.resolve(rootDir, 'public', 'javascripts') + path.sep;
@@ -48,12 +81,33 @@ module.exports = {
         lib: libDir,
         conf: confDir
     },
+    modulesVersion: {
+        client: pkgClient.version,
+        JSUS: pkgJSUS.version,
+        NDDB: pkgNDDB.version,
+        //shelf: ...,
+        window: pkgNgWindow.version,
+        widgets: pkgNgWidgets.version,
+        monitor: pkgMonitor.version,
+        requirements: pkgRequirements.version,
+        mturk: pkgMturk.version,
+        gameTemplate: pkgGameTemplate.version,
+        generator: pkgGenerator.version,
+        // Express and Socket.io
+        express: pkgExpress.version,
+        socketio: pkgSocketio.version
+    },
     modulesDir: {
         client: ngcDir,
         JSUS: JSUSDir,
         NDDB: NDDBDir,
         shelf: shelfDir,
         window: ngwindowDir,
-        widgets: ngwidgetsDir
+        widgets: ngwidgetsDir,
+        monitor: monitorDir,
+        requirements: requirementsDir,
+        mturk: mturkDir,
+        gameTemplate: gametemplateDir,
+        generator: generatorDir
     }
 };
