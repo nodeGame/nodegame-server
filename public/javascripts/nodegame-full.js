6713d4aacffe0b6ec50a03923918b8e26b3a0b6b
Stefano Balietti <futur.dorko@gmail.com>
2022-07-01 14:43:47 +0200

    built

--- public/javascripts/nodegame-full.js
+++ public/javascripts/nodegame-full.js
@@ -2268,7 +2268,7 @@ if (!Array.prototype.indexOf) {
      *   class, or undefined if the inputs are misspecified
      */
     DOM.removeClass = function(elem, className, force) {
-        var regexpr, o;
+        var regexpr;
         if (!force && !DOM.isElement(elem)) {
             throw new TypeError('DOM.removeClass: elem must be HTMLElement. ' +
                                 'Found: ' + elem);
@@ -2279,7 +2279,7 @@ if (!Array.prototype.indexOf) {
                                     'HTMLElement. Found: ' + className);
             }
             regexpr = new RegExp('(?:^|\\s)' + className + '(?!\\S)');
-            o = elem.className = elem.className.replace(regexpr, '' );
+            elem.className = elem.className.replace(regexpr, '' );
         }
         return elem;
     };
@@ -2654,7 +2654,7 @@ if (!Array.prototype.indexOf) {
      */
     DOM.blinkTitle = (function(id) {
         var clearBlinkInterval, finalTitle, elem;
-        clearBlinkInterval = function(opts) {
+        clearBlinkInterval = function() {
             clearInterval(id);
             id = null;
             if (elem) {
@@ -58012,113 +58012,6 @@ if (!Array.prototype.indexOf) {
         out = this.textareaElement ?
             this.textareaElement.value : this._feedback;
         return out ? out.trim() : out;
-    }
-
-})(node);
-
-/**
- * # Goto
- * Copyright(c) 2022 Stefano Balietti <ste@nodegame.org>
- * MIT Licensed
- *
- * Creates a simple interface to go to a step in the sequence.
- *
- * www.nodegame.org
- */
- (function(node) {
-
-    "use strict";
-
-    node.widgets.register('Goto', Goto);
-
-    // ## Meta-data
-
-    Goto.version = '0.0.2';
-    Goto.description = 'Creates a simple interface to move across ' +
-                       'steps in the sequence.';
-
-    Goto.title = false;
-    Goto.panel = false;
-    Goto.className = 'goto';
-
-    /**
-     * ## Goto constructor
-     *
-     * Creates a new instance of Goto
-     *
-     * @param {object} options Optional. Configuration options.
-     *
-     * @see Goto.init
-     */
-    function Goto() {
-        /**
-         * ### Goto.dropdown
-         *
-         * A callback executed after the button is clicked
-         *
-         * If it return FALSE, node.done() is not called.
-         */
-        this.dropdown;
-    }
-
-    Goto.prototype.append = function() {
-        this.dropdown = node.widgets.append('Dropdown', this.bodyDiv, {
-            tag: 'select',
-            choices: getSequence(),
-            id: 'ng_goto',
-            placeholder: 'Go to Step',
-            width: '15rem',
-            onchange: function(choice, datalist, that) {
-                node.game.gotoStep(choice);
-            }
-        });
-    };
-
-    /**
-     * ### Goto.disable
-     *
-     * Disables the widget
-     */
-    Goto.prototype.disable = function(opts) {
-        if (this.disabled) return;
-        this.disabled = true;
-        this.dropdown.enable();
-        this.emit('disabled', opts);
-    };
-
-    /**
-     * ### Goto.enable
-     *
-     * Enables the widget
-     */
-    Goto.prototype.enable = function(opts) {
-        if (!this.disabled) return;
-        this.disabled = false;
-        this.dropdown.disable();
-        this.emit('enabled', opts);
-    };
-
-
-    // ## Helper functions.
-
-    function getSequence(seq) {
-        var i, j, out, value, vvalue, name, ss;
-        out = [];
-        seq = seq || node.game.plot.stager.sequence;
-        for ( i = 0 ; i < seq.length ; i++) {
-            value = (i+1);
-            name = seq[i].id;
-            for ( j = 0 ; j < seq[i].steps.length ; j++) {
-                ss = seq[i].steps.length === 1;
-                vvalue = ss ? value : value + '.' + (j+1);
-                out.push({
-                    value: vvalue,
-                    name: vvalue + ' ' +
-                         (ss ? name : name + '.' + seq[i].steps[j])
-                });
-            }
-        }
-        return out;
     }
 
 })(node);
